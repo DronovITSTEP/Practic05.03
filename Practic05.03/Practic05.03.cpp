42c0b5b0a20646e8a5b5f6c348193824cbb5e654
ApxNord <dronov_a@t.top-academy.ru>
2023-03-10 21:03:58 +0300

    разбор шаблонов

--- Practic05.03/Practic05.03.cpp
+++ Practic05.03/Practic05.03.cpp
@@ -5,6 +5,37 @@
 
 using namespace std;
 
+template <class T>
+class Demo {
+    T _a;
+    T _b;
+public:  
+    Demo(){}
+    Demo(T a, T b) {
+        _a = a;
+        _b = b;
+    }   
+
+    void Add(){
+        cout << _a << " " << _b << endl;
+    }
+};
+
+template <typename T>
+void display(string p, T& value) {
+    cout << p;
+    cin >> value;
+
+    while (cin.fail()) {
+        cin.clear(); // восстанавливает поток ввода после ошибки
+        cin.ignore(32767, '\n'); // очищает буфер ввода
+
+        cout << p;
+        cin >> value;
+    }
+
+    cout << value;
+}
 
 
 
@@ -12,7 +43,7 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int y = 5;
+    /*int y = 5;
     int* p = &y;
     Book b{"автор 1", "книга 1" , (char*)"издатель 1", 2000,498};
     Book b2{"автор 1", "книга 2 автора 1" , (char*)"издатель 1", 2022,356};
@@ -29,5 +60,14 @@ int main()
 
     //fb.SaveBookFile(books, 5);
     Book* books2 = fb.LoadBookFile();
-    books2->ShowYearBook(cout, 2022, size);
+    books2->ShowYearBook(cout, 2022, size);*/
+
+    //int number;
+    //display("Enter number: ", number);
+
+    Demo<int> d{3, 5};
+    //d.Add();
+    Demo<int*> d2{ new int[3] {1,2,3},  new int[3] {1,2,3} };
+    d2.Add();
+
 }
